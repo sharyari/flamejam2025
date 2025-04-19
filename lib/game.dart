f6208e0988e6094ed46985d2298fb26ddcfdd7b0
Viktor Rådberg <viktor.raadberg@refapp.com>
2025-04-19 13:10:46 +0200

    consumable in game

--- lib/game.dart
+++ lib/game.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:spacegame/consumable.dart';
 import 'package:spacegame/player.dart';
 import 'package:spacegame/earth.dart';
 import 'package:flame/components.dart';
@@ -12,10 +13,12 @@ class SpaceGame extends FlameGame {
 class SpaceWorld extends World with HasCollisionDetection {
   Earth earth = Earth();
   Player player = Player();
+  Consumable consumable = Consumable(type: GenomeType.flight);
   @override
   FutureOr<void> onLoad() {
     add(earth);
     add(player);
+    add(consumable);
     debugMode = true;
     return super.onLoad();
   }
