0800e66a086c4e83a0e11e3cdf53a928713715de
zeyak <zeynep.hitay.akdeniz@gmail.com>
2025-04-20 15:34:46 +0200

    castle

--- lib/parallax_background.dart
+++ lib/parallax_background.dart
@@ -5,6 +5,7 @@ import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/parallax.dart';
+import 'package:flutter/widgets.dart';
 import 'package:spacegame/game.dart';
 
 class ParallaxBackground extends PositionComponent
@@ -13,13 +14,15 @@ class ParallaxBackground extends PositionComponent
   Future<void> onLoad() async {
     final parallax = await game.loadParallax(
       [
-        ParallaxImageData('backgrounds/ground_paralax_element_1.png'),
-        ParallaxImageData('backgrounds/ground_paralax_element_2.png'),
         ParallaxImageData('backgrounds/ground_paralax_element_3.png'),
-        ParallaxImageData('backgrounds/ground_paralax_element_4.png'),
+        //ParallaxImageData('backgrounds/ground_paralax_element_4.png'),
+        //ParallaxImageData('backgrounds/ground_paralax_element_1.png'),
+        //ParallaxImageData('backgrounds/ground_paralax_element_2.png'),
       ],
       velocityMultiplierDelta: Vector2(1.2, 0),
+      repeat: ImageRepeat.noRepeat,
     );
+
     size = Vector2(2732, 700);
     game.world.player.velocity.addListener(() {
       parallax.baseVelocity.setFrom(game.world.player.velocity / 2);
