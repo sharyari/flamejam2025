a50d2d0730a91e78ae6411413d28b87fe281d0a2
zeyak <zeynep.hitay.akdeniz@gmail.com>
2025-04-20 14:45:14 +0200

    earth_fixed

--- lib/earth.dart
+++ lib/earth.dart
@@ -1,21 +1,27 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
+import 'package:flame/parallax.dart';
 
-class Earth extends SpriteComponent with HasGameReference {
+class Earth extends PositionComponent with HasGameReference {
   @override
   Future<void> onLoad() async {
-    final image = await Flame.images.load('backgrounds/ground_tile_1.png');
-    sprite = Sprite(
-      image,
-      srcPosition: Vector2(0, 0),
-      srcSize: Vector2(2732, 2048),
+    final parallax = await game.loadParallax(
+      [
+        ParallaxImageData('backgrounds/ground_tile_1.png'),
+      ],
+      filterQuality: FilterQuality.none,
     );
-    size = Vector2(2732, 203);
+    size = Vector2(2732, 50);
+
+    final groundParallax = ParallaxComponent(parallax: parallax);
     anchor = Anchor.bottomCenter;
     position.y = game.size.y;
     add(RectangleHitbox());
+
+    add(groundParallax);
   }
 }
