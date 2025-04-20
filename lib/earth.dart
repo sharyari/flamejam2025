705c9dcbb1351831924f327ad92fe71c240bb9cd
Viktor Rådberg <viktor.raadberg@refapp.com>
2025-04-20 11:48:32 +0200

    ground

--- lib/earth.dart
+++ lib/earth.dart
@@ -2,15 +2,20 @@ import 'dart:async';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flutter/material.dart';
+import 'package:flame/flame.dart';
 
-class Earth extends RectangleComponent with HasGameReference {
+class Earth extends SpriteComponent with HasGameReference {
   @override
   Future<void> onLoad() async {
-    size = Vector2(game.size.x * 20, game.size.y * 0.6);
+    final image = await Flame.images.load('backgrounds/ground_tile_1.png');
+    sprite = Sprite(
+      image,
+      srcPosition: Vector2(0, 0),
+      srcSize: Vector2(2732, 2048),
+    );
+    size = Vector2(2732, 2048);
     anchor = Anchor.bottomCenter;
     position.y = game.size.y;
-    paint.color = Colors.green;
     add(RectangleHitbox());
   }
 }
